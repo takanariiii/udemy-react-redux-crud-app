3191ea5822bd0241774f98e886b30271649cd28c
arai.ryusei <r.sca.1207.takanari@gmail.com>
2020-05-01 01:37:23 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return <h1>Hello World! </h1>
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => { console.log("I am clicked!") }} />
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
-
