2a6ba3fa4428773c1e29c49b8b2352033bcaca9c
arai.ryusei <r.sca.1207.takanari@gmail.com>
2020-05-01 14:32:57 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREMEMT'
+export const DECREMENT = 'DECREMEMT'
+
+export const increment = () => ({
+    type: 'INCREMENT'
+})
+
+export const decrement = () => ({
+    type: 'DECREMENT'
+})
